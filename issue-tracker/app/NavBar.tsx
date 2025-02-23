63e5624832ecf5fea144aa16c003ddc7b0c01736
Melissa Usher <MelEUsher17@gmail.com>
2025-02-23 01:12:09 -0600

    formatted page to make it easier to read

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -11,9 +11,11 @@ const NavBar = () => {
     <nav className= 'flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/"><GiLadybug /></Link>
         <ul className= 'flex space-x-6'>
-            {links.map(link => <Link key={link.href} 
-            className= 'text-zinc-500 hover: text-zinc-800 transition-colors'
-            href= {link.href}>{link.label}</Link>)}
+            {links.map(link => 
+            <Link 
+            key={link.href} 
+            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
   )
