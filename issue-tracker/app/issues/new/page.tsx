0b1a12c8ce93d5b9b528b4f8c807d1d855f784b1
Melissa Usher <MelEUsher17@gmail.com>
2025-02-28 17:07:59 -0600

    recreated env file because it disappeared

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,12 +1,11 @@
-'use client';
+"use client";
 
-import { TextField, Button } from '@radix-ui/themes';
+import { TextField, Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from 'react-hook-form';
-import axios from 'axios'; 
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import { useRouter } from 'next/navigation';
-
+import { useRouter } from "next/navigation";
 
 interface IssueForm {
   title: string;
@@ -15,27 +14,27 @@ interface IssueForm {
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const {register, control, handleSubmit} = useForm<IssueForm>();
-    
-  return (
-    <form className= 'max-w-xl space-y-3' 
-        onSubmit = {handleSubmit (async (data) => {
-        
-          await axios.post ('/api/issues', data);
-            router.push ('/issues');
-          
-           })}
-    >
-        <TextField.Root placeholder='Title' {...register ('title')} />
-        <Controller
-          name = 'description'
-          control = {control}
-          render = {({field}) => <SimpleMDE placeholder = 'Description' {...field} />}
-       />
-        <Button>Submit New Issue</Button>
-    
-    </form>
-  )
-}
+  const { register, control, handleSubmit } = useForm<IssueForm>();
 
-export default NewIssuePage
+  return (
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+      <Button>Submit New Issue</Button>
+    </form>
+  );
+};
+
+export default NewIssuePage;
