6559ca899ad4fa51fc786d95526bc33cc0f19f54
Melissa Usher <MelEUsher17@gmail.com>
2025-02-27 21:24:07 -0600

    Supposed to be included in last commit

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
-import { TextField } from '@radix-ui/themes';
+import { TextField, TextArea, Button } from '@radix-ui/themes';
 import React from 'react';
 
 const NewIssuePage = () => {
   return (
-    <div>
+    <div className= 'max-w-xl space-y-3'>
+    
         <TextField.Root placeholder='Title' />
+        <TextArea placeholder = 'Description'/>
+        <Button>Submit New Issue</Button>
     
     </div>
   )
